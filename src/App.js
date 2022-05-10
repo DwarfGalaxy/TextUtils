bf35d7d6d8fbd6c977c0bae4e7480376731ba45c
Yubraj <nontoxicyubs@gmail.com>
2022-05-10 14:48:22 +0545

    New textutils website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
 
 function App() {
+  const [background_Color, setbackground_Color] = useState('light');
+  const [text_Color, setText_Color] = useState('dark');
+  const [textareaColor, set_textareaColor] = useState('light');
+  const [modeText, setmodeText] = useState("Enable Dark Mode");
+  const [alert, setalert] = useState(null);
+  const showAlert = (type, message) => {
+    setalert({
+      type: type,
+      message: message
+    });
+    setTimeout(() => {
+      setalert(null);
+    }, 2000);
+  }
+
+  let changeBackgroundColor = () => {
+    if (background_Color == "light") {
+      setbackground_Color("dark");
+      setmodeText("Enable Light Mode");
+      setText_Color("light");
+      set_textareaColor("Secondary");
+      document.body.style.backgroundColor = "#0f5132";
+      document.body.style.color = "white";
+
+      showAlert("Success!", "Dark mood has been enabled", "show");
+
+      document.title = "TextUtils - Dark mode enabled";
+      // setInterval(() => {
+      //   document.title="Danger!!!!!!!!!!!!!!!!!";
+      // }, 1000);
+      // setInterval(() => {
+      //   document.title="Computer has been hacked!";
+      // }, 2000);
+    } else {
+      setbackground_Color("light");
+      setmodeText("Enable Dark Mode");
+      setText_Color("dark");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      set_textareaColor("light");
+
+      showAlert("Success!", "Light mode has been enabled", "show");
+      document.title = "TextUtils - Light mode enabled";
+      // setInterval(() => {
+      //   document.title="Danger!!!!!!!!!!!!!!!!!";
+      // }, 1000);
+      // setInterval(() => {
+      //   document.title="Computer has been hacked!";
+      // }, 2000);
+    }
+  }
   return (
+    <>
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+      <Navbar title="TextUtils" about="About" background_Color={background_Color} text_Color={text_Color} modeText={modeText} changeBackgroundColor={changeBackgroundColor} />
+      <Alert alert={alert} />
+      <Routes>
+        <Route path="/" element={
+          <div className="container my-4">
+          <Textform heading="TextUtils - Word Counter, Character Counter, Remove extra spaces" textareaColor={textareaColor} />
+        </div>
+        } />
+        <Route path="about" element={<About />} />
+      </Routes>
+      </div>
+      </BrowserRouter>
+    </>
+
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react';
+
+
+export default function Textform(props) {
+    const [comments, setComments] = useState('');
+    let handleOnChange=(events)=>{
+        setComments(events.target.value);
+    }
+    let toUpperCase=()=>{
+        let newText=comments.toUpperCase();
+        setComments(newText);
+    }
+    let toLowerCase=()=>{
+        let newText=comments.toLowerCase();
+        setComments(newText);
+    }
+    let clearText=()=>{
+        setComments("");
+    }
+    
+    return (
+        <>
+            <h2>{props.heading}</h2>
+            <div className="form">
+                <textarea className={`form-control bg-${props.textareaColor}`}  value={comments} onChange={handleOnChange} rows="8" id='textMessage'></textarea>
+                <button type="button" className="btn btn-primary my-2" onClick={toUpperCase} >Convert to UpperCase</button>
+                <button type="button" className="btn btn-primary mx-2" onClick={toLowerCase}>Convert to lowerCase</button>
+                <button type="button" className="btn btn-primary mx-2" onClick={clearText}>Clear Text</button>
+            </div>
+
+            <div className="container">
+                <h1>Your text Summary</h1>
+                <p>{comments.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words  </p>
+                <p>{(comments.trim().split(/\s+/).length)*0.008} minutes to read.</p>
+                <h2>Preview:</h2>
+                <p>{comments}</p>
+
+            </div>
+        </>
+    )
+}
